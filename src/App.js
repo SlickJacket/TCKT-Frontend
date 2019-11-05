b551c7c9c5304a31bcb6746018b4c0c611f38a9c
SlickJacket <novenarii@gmail.com>
2019-11-05 16:37:20 -0500

    main styling done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import CheckoutForm from './CheckoutForm';
+import { PaymentRequestDemo } from './_PaymentRequestForm';
+import {Elements, StripeProvider} from 'react-stripe-elements';
+import logo from './TCKT.png';
 
-function App() {
+class App extends Component {
+
+  state = {
+    eventName: ""
+  }
+
+  componentDidMount() {
+    fetch('https://27nw6pkrj3.execute-api.us-east-1.amazonaws.com/default/getEvent/2') 
+    .then(res => res.json())
+    .then(data => this.setState({eventName: data[0].eventName}))
+  }
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <StripeProvider apiKey="pk_test_E1clmPG3gbZZ4QatxXyn2Dbr00kNJFsGqn">
+    <div className="example">
+      <div id="eventHeader">
+          <h1 id='eventName'>{this.state.eventName}</h1>
+          <div id="line"></div>
+        
+        <div id='venueAndInfoContainer'>
+          <h1 id='venueName'>THE CO-OP</h1>
+          <h1 id="neighborhood">Bed-Stuy, BK</h1>
+          <div id='address'>
+            {/* <h3 style={{color: "white"}}>168 Ralph Ave Brooklyn,</h3>
+            <h3 style={{color: "white"}}>NY 11233</h3> */}
+          </div>
+        </div>
+        
+        <div id='logoContainer'>
+          <img src={logo} id='logo' />
+        </div>
+
+          <div id='icons'>
+              <div id="categoryIcon">
+                <h1 id="categoryAbb">C</h1>
+              </div>
+              
+
+              <div id="pricediv">
+                <h2 id="priceLabel">$10</h2>
+              </div>
+
+              <div id='timediv'>
+                <h2 id='time'>9PM</h2>
+              </div>
+          </div>
+
+      </div>
+
+      
+
+      <div id="payButton">
+      <Elements>
+        <PaymentRequestDemo />
+      </Elements>
+      </div>
     </div>
+  </StripeProvider>
   );
 }
-
+}
 export default App;
